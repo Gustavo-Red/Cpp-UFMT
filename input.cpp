ee93eb21a4809e18d932f04140b7d270939f18ed
Gustavo-red <spideyred@proton.me>
2026-08-17 19:05:40 -0300

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout << "Digite seu nome: ";
+    cin >> name;
+    cout << "Seu nome: " << name << endl;
+    return 0;
+}
+
+
+
